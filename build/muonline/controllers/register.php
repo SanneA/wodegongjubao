e37c4fecc780a5561f6f5f764dd177623ca08781
epmak <epmak@1316c22d-e87f-b044-9b9b-93d7a3e3ba9c>
2015-11-21 13:23:07 +0000

    добавлена регистрация с почтой исправлена проблема с добавлением 0 в значении в конфиг добавлены параметры к регистрации

--- build/muonline/controllers/register.php
+++ build/muonline/controllers/register.php
@@ -30,7 +30,13 @@ class register extends muController
 
     public function action_setregister()
     {
-        if(!empty($_POST["rlogin"]) && !empty($_POST["rpwd"]) && !empty($_POST["rrpwd"]) && !empty($_POST["rmail"]) && !empty($_POST["rcaptch"]))
+        if($this->configs["useCaptcha"] == 1 && empty($_POST["rcaptch"])) // если капча вкоючена
+        {
+            echo "0::".$this->view->getVal("l_err4");
+            return;
+        }
+
+        if(!empty($_POST["rlogin"]) && !empty($_POST["rpwd"]) && !empty($_POST["rrpwd"]) && !empty($_POST["rmail"]))
         {
             $login = $_POST["rlogin"];
             if(!preg_match("#^[aA-zZ0-9\-_]+$#",$login))
@@ -55,14 +61,18 @@ class register extends muController
 
 
             $email = $_POST["rmail"];
-            $cp = $_POST["rcaptch"];
 
-            if($cp != $_SESSION["captcha_keystring"])
+            if($this->configs["useCaptcha"]!=0)
             {
-                echo "0::".$this->view->getVal("l_err4");
-                return;
-            }
+                $cp = $_POST["rcaptch"];
 
+                if($cp != $_SESSION["captcha_keystring"])
+                {
+                    echo "0::".$this->view->getVal("l_err4");
+                    return;
+                }
+                unset($_SESSION["captcha_keystring"]);
+            }
 
 
             if(!$this->model->checkLogin($login))
@@ -71,10 +81,67 @@ class register extends muController
                 return;
             }
 
-            $this->model->reg($login,$pwd,$email);
-            unset($_SESSION["captcha_keystring"]);
+            //l_err7
+            if($this->configs["useMail"]==0)
+            {
+                $this->model->reg($login,$pwd,$email);
+                echo "1::".$this->view->getVal("l_err6");
+            }
+            else
+            {
+                $id = $this->model->regM($login,$pwd,$email,$this->configs["defGrp"]);
+                if($id>0)
+                {
+                    //region отсылаем почту
+                    $hash = md5($id."-=-".$email);
+                    $c_mail = Configs::readCfg("mail","muonline");
 
-            echo "1::".$this->view->getVal("l_err6");
+                    require "libraries/PHPMailer/PHPMailerAutoload.php";
+
+                    $pop = new POP3();
+                    $pop->Authorise($c_mail["mailhost"],$c_mail["mailport"],$c_mail["mailtmout"], $c_mail["mailboxf"], $c_mail["mailpbf"], $c_mail["maildlvl"]);
+                    $mail = new PHPMailer();
+                    $mail->CharSet="UTF-8";
+                    //  $mail->IsSMTP();
+                    //$mail->SMTPDebug  = $c_mail["maildlvl"];
+                    $mail->Host       = $c_mail["mailhost"];
+                    $mail->SMTPAuth   = true;
+                    $mail->Username   = $c_mail["mailboxf"];
+                    $mail->Password   = $c_mail["mailpbf"];
+
+                    $mail->SetFrom($c_mail["mailboxf"], $c_mail["mailnamefrom"]);
+                    $mail->AddReplyTo($c_mail["mailboxf"],$c_mail["mailnib"]);
+                    $mail->Subject    = "Register";
+                    $mail->AltBody    = "To view the message, please use an HTML compatible email viewer!"; // optional, comment out and test
+
+                    $this->view->set("hash",$hash."_".$id);
+                    $mail->MsgHTML($this->view->out("register","mail",2));
+                    $mail->AddAddress($email);
+
+                    if(!$mail->Send())
+                        $this->model->toLog("Mail error:".$mail->ErrorInfo,"register",13);
+
+                    //endregion
+
+                    echo "1::".$this->view->getVal("l_err7");
+                }
+                else
+                    echo "0::".$this->view->getVal("l_err5");
+            }
+
+        }
+    }
+
+    /**
+     * активация аккаунта
+     */
+    public function action_activate()
+    {
+        if(!empty($_GET["get"]))
+        {
+            $params = explode("_",$_GET["get"]);
+            if($this->model->activate((int)$params[1],$params[0]))
+                Tools::go($this->view->getAdr());
         }
     }
 }
--- build/muonline/models/m_register.php
+++ build/muonline/models/m_register.php
@@ -24,6 +24,13 @@ class m_register extends MuonlineUser
         return false;
     }
 
+    /**
+     * регистрация. простая
+     * @param string $login
+     * @param string $pwd
+     * @param string $mail
+     * @throws ADODB_Exception
+     */
     public function reg($login,$pwd,$mail)
     {
         if((int)$this->cfg["usemd5"]>0)
@@ -33,5 +40,45 @@ class m_register extends MuonlineUser
 
         $this->db->query("INSERT INTO memb_info (memb___id,memb_name,memb__pwd,mail_addr,sno__numb,bloc_code,ctl1_code) VALUES('$login','$login', $r_password,'$mail',1,0,1)");
         $this->db->SQLog("registered new account: $login","register",7);
+    }
+
+    /**
+     * регистрация, с использованием активации на почту
+     * @param string $login
+     * @param string $pwd
+     * @param string $mail
+     * @param int $group
+     * @return int ид пользователя
+     * @throws ADODB_Exception
+     */
+    public function regM($login,$pwd,$mail,$group)
+    {
+        if((int)$this->cfg["usemd5"]>0)
+            $r_password = "[dbo].[fn_md5]('{$pwd}','{$login}')";
+        else
+            $r_password = "'".$pwd."'";
+
+        $this->db->query("INSERT INTO memb_info (memb___id,memb_name,memb__pwd,mail_addr,sno__numb,bloc_code,ctl1_code,MWCpoints) VALUES('$login','$login', $r_password,'$mail',1,0,1,$group)");
+        $id = $this->db->lastId("memb_info");
+
+        $this->db->SQLog("registered new account: $login","register",7);
+        return $id;
+    }
+
+    /**
+     * @param int $id
+     * @param string $hash
+     * @return bool
+     * @throws ADODB_Exception
+     */
+    public function activate($id,$hash)
+    {
+        $info = $this->db->query("SELECT mail_addr FROM memb_info WHERE memb_guid=$id")->FetchRow();
+        if(md5($id."-=-".$info["mail_addr"]) == $hash)
+        {
+            $this->db->query("UPDATE memb_info SET MWCpoints = 5 WHERE memb_guid=$id AND MWCpoints !=5");
+            return true;
+        }
+        return false;
     }
 }
