9d124d42e8cb1fc4ca3b2cf7edb8d0b515ea1877
epmak <epmak@1316c22d-e87f-b044-9b9b-93d7a3e3ba9c>
2015-12-02 13:24:19 +0000

    доработка недоработанного

--- build/admin/plugins/model/m_menu.php
+++ build/admin/plugins/model/m_menu.php
@@ -7,7 +7,7 @@
  * 31.08.2015
  *
  **/
-class m_menu extends Model
+class m_menu extends Menu
 {
     /**
      * выбирает из базы менюшку и возвращает ее
@@ -15,26 +15,6 @@ class m_menu extends Model
      */
     public function getMenu()
     {
-        $path = "build".DIRECTORY_SEPARATOR.$_SESSION["mwcabuild"].DIRECTORY_SEPARATOR."lang".DIRECTORY_SEPARATOR.$_SESSION["mwclang"].DIRECTORY_SEPARATOR."titles.php";
-        if(file_exists($path))
-            require $path;
-        else
-            $lang = array();
-
-        $return = array();
-        $i=0;
-        //$q = $this->db->query("SELECT id,mtitle,mtype,link,server,modul FROM mwc_menu WHERE tbuild='{$_SESSION["mwcabuild"]}' and server={$_SESSION["mwcserver"]} AND mtype = 2"); //выбираем меню админа только для нашего билда
-        $q = $this->db->query("SELECT mm.id,mm.mtitle,mm.mtype,mm.link,mm.server,mm.modul FROM mwc_menu mm, mwc_menu_type mmt WHERE  mm.mtype = mmt.id AND mmt.tbuild='admin' AND mmt.ttitle='adminmenu' order by mm.col_Seq"); //выбираем меню админа только для нашего билда
-        while ($r = $q->FetchRow())
-        {
-            if(!empty($lang[$r["mtitle"]]))
-                $r["mtitle"] = $lang[$r["mtitle"]];
-            $r["link"] = Tools::linkDec($r["link"]); //снимает экранирование с амперсанда
-            $return[$i] = $r;
-
-            $i++;
-        }
-
-        return $return;
+        return parent::getMenu('admin','adminmenu');
     }
 }
