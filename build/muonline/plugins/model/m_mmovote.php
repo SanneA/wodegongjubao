ae35eba9ed26a0c203ff7b68121b85baec510641
epmak <epmak@1316c22d-e87f-b044-9b9b-93d7a3e3ba9c>
2015-12-25 14:15:05 +0000

    переработан парсер топа переработан плагин голосования

--- build/muonline/plugins/model/m_mmovote.php
+++ build/muonline/plugins/model/m_mmovote.php
@@ -16,24 +16,24 @@ class m_mmovote extends MuonlineUser
         $ai = new ArrayIterator($array);
         foreach ($ai as $ar)
         {
-            if (!empty($ar) && isset($ar[4]) && isset($ar[3]) && isset($ar[1]) && strlen(trim($ar[3]))<=10)
+            if (!empty($ar) && isset($ar["vote"]) && isset($ar["acc"]) && isset($ar["date"]) && strlen(trim($ar["acc"]))<=10)
             {
-                $ar[4] =  str_replace("\n", "", trim($ar[4]));
-                $credist = str_replace(array("(votes)","(account)"), array("{$ar[4]}","'{$ar[3]}'"),$configs["mmo_price"]);
-                $ar[1] = date_::intransDate($ar[1],true);
-                $q.="IF EXISTS (SELECT * FROM [MWC_MMO_TOP] WHERE col_memb_id = '{$ar[3]}')
+                $ar["vote"] =  str_replace("\n", "", trim($ar["vote"]));
+                $credist = str_replace(array("(votes)","(account)"), array("{$ar["vote"]}","'{$ar["acc"]}'"),$configs["mmo_price"]);
+                $ar["date"] = date_::intransDate($ar["date"],true);
+                $q.="IF EXISTS (SELECT * FROM [MWC_MMO_TOP] WHERE col_memb_id = '{$ar["acc"]}')
 BEGIN
-  IF EXISTS (SELECT * FROM [MWC_MMO_TOP] WHERE col_memb_id = '{$ar[3]}' AND DATEDIFF(HOUR,col_LastVote,'".$ar[1]."')>=1)
+  IF EXISTS (SELECT * FROM [MWC_MMO_TOP] WHERE col_memb_id = '{$ar["acc"]}' AND DATEDIFF(HOUR,col_LastVote,'".$ar["date"]."')>=1)
   BEGIN
-    UPDATE [MWC_MMO_TOP] SET col_votes = col_votes + {$ar[4]},col_LastVote='".$ar[1]."' WHERE col_memb_id = '{$ar[3]}';
-    INSERT INTO MWC_logs(col_ErrNum,col_msg,col_mname,col_createTime) VALUES(42,'Account {$ar[3]} add 1 vote from mmo top','vote',GETDATE());
+    UPDATE [MWC_MMO_TOP] SET col_votes = col_votes + {$ar["vote"]},col_LastVote='".$ar["date"]."' WHERE col_memb_id = '{$ar["acc"]}';
+    INSERT INTO MWC_logs(col_ErrNum,col_msg,col_mname,col_createTime) VALUES(42,'Account {$ar["acc"]} add 1 vote from mmo top','vote',GETDATE());
     $credist;
   END
 END
 ELSE
 BEGIN
-  INSERT INTO [MWC_MMO_TOP] (col_memb_id, col_LastVote,col_votes) VALUES('{$ar[3]}','".$ar[1]."',{$ar[4]});
-  INSERT INTO mwce_settings.dbo.MWC_logs(col_ErrNum,col_msg,col_mname,col_createTime,tbuild) VALUES(15,'Account {$ar[3]} add {$ar[4]} vote from mmo top ','vote',GETDATE(),'".tbuild."');
+  INSERT INTO [MWC_MMO_TOP] (col_memb_id, col_LastVote,col_votes) VALUES('{$ar["acc"]}','".$ar["date"]."',{$ar["vote"]});
+  INSERT INTO mwce_settings.dbo.MWC_logs(col_ErrNum,col_msg,col_mname,col_createTime,tbuild) VALUES(15,'Account {$ar["acc"]} add {$ar["vote"]} vote from mmo top ','vote',GETDATE(),'".tbuild."');
   $credist;
 END ";
                 $contis++;
